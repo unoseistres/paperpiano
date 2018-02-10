34becac50be3b2418aa27d00c4aef630be815939
Dave Myers <brahmafear@gmail.com>
2018-02-09 20:53:28 -0500

    Reworked code

--- paperPianoPentatonic/paperPianoPentatonic.ino
+++ paperPianoPentatonic/paperPianoPentatonic.ino
@@ -1,55 +1,94 @@
-// paper piano: here we divide an octave into the notes of the pentatonic scale
+/**
+ * Paper Piano
+ * A demonstration of the pentatonic scale using Arduino, conductive paint, and paper.
+ * 
+ * 
+ * 
+ * 
+ */
 
-int sensorPin = A0;    // input pin
-unsigned int sensorValue = 0;   // variable to store the value coming from the sensor
-unsigned int sensorAvg = 0;
-int step = 0;
-int offset = 5; // if key readings are very close together in value, decrease this to 3 or so
+// --------------------------------------------------------------------------------------
+// Paper readings:
+//   Change these settings based on the readings from the individual keys of the piano.
+//   Place values in descending order regardless of key readings.
+//float keyReadings[] = {377, 340, 311, 279, 247};
+#define   PP_KEY_1            377
+#define   PP_KEY_2            340
+#define   PP_KEY_3            311
+#define   PP_KEY_4            279
+#define   PP_KEY_5            247
 
-// pentatonic scale- A4-A5
-int octave = 1; // default is 1. Change to 2 or 4 for higher octaves!
-float scale[] = {440*octave, 495*octave, 556.875*octave, 660*octave, 742.5*octave}; // array storing frequencies for pentatonic scale
+// --------------------------------------------------------------------------------------
+// Sound and sensitivity configuration:
+//   These settings determine the sensitivity of values as well as the octave produced
+#define   PP_OCTAVE           1       // Change to 2 or 4 for higher pitches
+#define   PP_SHOULDER         5       // Reduce this to 3 if measured values are close together
+#define   PP_SILENT_CUTOFF    30      // Value below which should be considered no keys pressed
 
-// REPLACE with readings from the 5 keys
-float keyReadings[] = {377, 340, 311, 279, 247};
+// --------------------------------------------------------------------------------------
+// Scale configuration:
+//   The defaults create frequencies for the pentatonic scale.
+#define   PP_TONE_1           440
+#define   PP_TONE_2           495
+#define   PP_TONE_3           556.875
+#define   PP_TONE_4           660
+#define   PP_TONE_5           742.5
+
+// --------------------------------------------------------------------------------------
+// Hardware configuration:
+//   Change these settings based on the configuration of the hardware
+
+#define   PP_ANALOG_INPUT     A0
+#define   PP_SPEAKER_OUTPUT   11
+#define   PP_SERIAL_BAUD      2400
+#define   PP_READ_COUNT       100
+
+
+// --------------------------------------------------------------------------------------
+// Arduino code
+//   No changes should be made below this line
 
 void setup() {
-  // initialize serial communications at 2400 bps:
-  Serial.begin(2400);
+  Serial.begin( PP_SERIAL_BAUD );
 }
 
 void loop() {
-
-  // read the value from the sensor:
-  sensorValue += analogRead(sensorPin);
-  step = (step+1)%100;
-  if(step == 0){
-    sensorAvg = sensorValue/100;
-    sensorValue = 0;
-    Serial.println(sensorAvg);
-  } 
   
-  if(sensorAvg < 30){
-    noTone(11);
+  uint32_t sensorValue = 0;
+  
+  // Take several analog readings to create average
+  for ( int i = 0; i < PP_READ_COUNT; i++ ) {
+    sensorValue += analogRead( PP_ANALOG_INPUT );
   }
-  else if(sensorAvg < keyReadings[4]+offset){
-    tone(11, scale[4]);
+
+  // Calculate average reading
+  sensorValue /= PP_READ_COUNT;
+  
+  // Print out analog reading
+  Serial.println( sensorValue );
+
+  // Produce tone based on analog input
+  if ( sensorValue < PP_SILENT_CUTOFF ) {
+    noTone( PP_SPEAKER_OUTPUT );
   }
-  else if(sensorAvg < keyReadings[3]+offset){
-    tone(11, scale[3]);
+  else if ( sensorValue < PP_TONE_5 + PP_SHOULDER ) {
+    tone( PP_SPEAKER_OUTPUT, PP_TONE_5 * PP_OCTAVE );
   }
-  else if(sensorAvg < keyReadings[2]+offset){
-    tone(11, scale[2]);
+  else if ( sensorValue < PP_TONE_4 + PP_SHOULDER ) {
+    tone( PP_SPEAKER_OUTPUT, PP_TONE_4 * PP_OCTAVE );
   }
-  else if(sensorAvg < keyReadings[1]+offset){ 
-    tone(11, scale[1]);
+  else if ( sensorValue < PP_TONE_3 + PP_SHOULDER ) {
+    tone( PP_SPEAKER_OUTPUT, PP_TONE_3 * PP_OCTAVE );
   }
-  else if(sensorAvg < keyReadings[0]+offset){
-    tone(11, scale[0]);
-  }  else{
- noTone(11);
+  else if ( sensorValue < PP_TONE_2 + PP_SHOULDER ) {
+    tone( PP_SPEAKER_OUTPUT, PP_TONE_2 * PP_OCTAVE );
+  }
+  else if ( sensorValue < PP_TONE_1 + PP_SHOULDER ) {
+    tone( PP_SPEAKER_OUTPUT, PP_TONE_1 * PP_OCTAVE );
+  }
+  else {
+    noTone( PP_SPEAKER_OUTPUT );
   }
   
 }
-
 
